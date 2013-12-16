154051a5ffa46f8c081a060e1e71ff1a543c5dd1
James Marca <jmarca@translab.its.uci.edu>
2013-12-16 09:29:47 -0800

    added a switch to allow overwriting.  plus tests of same
    modified:   lib/put_doc.js
    modified:   test/test_put_doc.js

--- test/test_put_doc.js
+++ test/test_put_doc.js
@@ -21,86 +21,75 @@ console.log('testing couchdb='+couch)
  * create a test db, and populate it with data
  * and with a view
  *
- * Instead, because I want to get some real work done, this test is
- * going to just use an exsiting db that I have with existing views
- *
- * If your name is not James E. Marca, you should change this
- *
  */
 
 var doc = {"_id": "801245",
-                     "2006": {
-                     },
-                     "2007": {
-                         "vdsimputed": "todo",
-                         "wim_neigbors_ready": {
-                             "wim_id": 77,
-                             "distance": 14788,
-                             "direction": "east"
-                         },
-                         "wim_neigbors": {
-                             "wim_id": 77,
-                             "distance": 14788,
-                             "direction": "east"
-                         },
-                         "truckimputed": "2013-04-06T04:45:11.832Z finish",
-                         "paired_wim": null,
-                         "vdsdata": "0",
-                         "rawdata": "1",
-                         "row": 1,
-                         "vdsraw_chain_lengths": [2,2,2,2,2],
-                         "vdsraw_max_iterations": 0,
-                         "occupancy_averaged": 1,
-                         "truckimputation_chain_lengths": [
-                             145,
-                             147,
-                             144,
-                             139,
-                             143
-                         ],
-                         "truckimputation_max_iterations": 0
-                     },
-                     "2008": {
-                         "vdsimputed": "todo",
-                         "wim_neigbors_ready": {
-                             "wim_id": 77,
-                             "distance": 14788,
-                             "direction": "east"
-                         },
-                         "wim_neigbors": {
-                             "wim_id": 77,
-                             "distance": 14788,
-                             "direction": "east"
-                         },
-                         "vdsdata": "0",
-                         "rawdata": "1",
-                         "row": 1,
-                         "truckimputed": "2012-05-21 inprocess",
-                         "vdsraw_chain_lengths": [2,2,2,2,2],
-                         "vdsraw_max_iterations": 0
-                     }}
+           "2006": {
+           },
+           "2007": {
+               "vdsimputed": "todo",
+               "wim_neigbors_ready": {
+                   "wim_id": 77,
+                   "distance": 14788,
+                   "direction": "east"
+               },
+               "wim_neigbors": {
+                   "wim_id": 77,
+                   "distance": 14788,
+                   "direction": "east"
+               },
+               "truckimputed": "2013-04-06T04:45:11.832Z finish",
+               "paired_wim": null,
+               "vdsdata": "0",
+               "rawdata": "1",
+               "row": 1,
+               "vdsraw_chain_lengths": [2,2,2,2,2],
+               "vdsraw_max_iterations": 0,
+               "occupancy_averaged": 1,
+               "truckimputation_chain_lengths": [
+                   145,
+                   147,
+                   144,
+                   139,
+                   143
+               ],
+               "truckimputation_max_iterations": 0
+           },
+           "2008": {
+               "vdsimputed": "todo",
+               "wim_neigbors_ready": {
+                   "wim_id": 77,
+                   "distance": 14788,
+                   "direction": "east"
+               },
+               "wim_neigbors": {
+                   "wim_id": 77,
+                   "distance": 14788,
+                   "direction": "east"
+               },
+               "vdsdata": "0",
+               "rawdata": "1",
+               "row": 1,
+               "truckimputed": "2012-05-21 inprocess",
+               "vdsraw_chain_lengths": [2,2,2,2,2],
+               "vdsraw_max_iterations": 0
+           }}
 
 describe('put',function(){
-    var created_locally=false
     before(function(done){
         // create a test db, the put data into it
         superagent.put(couch)
         .auth(cuser,cpass)
         .end(function(e,r){
-            r.should.have.property('error',false)
-            if(!e)
-                created_locally=true
-                return done()
-            })
+            return done()
+        })
         return null
     })
 
     after(function(done){
-        if(!created_locally) return done()
-
         // bail in development
         //console.log(couch)
-        return done()
+        // return done()
         var opts = {'uri':couch
                    ,'method': "DELETE"
                    ,'headers': {}
@@ -144,6 +133,61 @@ describe('put',function(){
             r.should.have.property('rev')
             r.should.have.property('id')
             done()
+        })
+
+    })
+    it('should not be able to overwrite an existing doc unless told to do so',function(done){
+        var putter = putdoc({cdb:test_db,
+                             cuser:cuser,
+                             cpass:cpass,
+                             chost:chost,
+                             cport:cport
+                            })
+        putter.overwrite(false)
+        var testdoc = {'_id':'testingnot',
+                       'value':'banana'
+                      }
+        var doc_replace = {_id:testdoc._id,
+                           'pancake':'breakfast'}
+        putter(testdoc,function(e,r){
+            should.not.exist(e)
+            r.should.have.property('ok')
+            r.should.have.property('rev')
+            r.should.have.property('id',testdoc._id)
+            putter(doc_replace,function(e,r){
+                r.should.not.have.property('ok')
+                r.should.not.have.property('rev')
+                r.should.not.have.property('id')
+                r.should.have.property('error','conflict')
+                done()
+            })
+        })
+
+    })
+    it('should be able to overwrite an existing doc',function(done){
+        var putter = putdoc({cdb:test_db,
+                             cuser:cuser,
+                             cpass:cpass,
+                             chost:chost,
+                             cport:cport
+                            })
+        putter.overwrite(true)
+        var testdoc = {'_id':'testing',
+                       'value':'banana'
+                      }
+        var doc_replace = {_id:testdoc._id,
+                           'pancake':'breakfast'}
+        putter(testdoc,function(e,r){
+            should.not.exist(e)
+            r.should.have.property('ok')
+            r.should.have.property('rev')
+            r.should.have.property('id',testdoc._id)
+            putter(doc_replace,function(e,r){
+                r.should.have.property('ok')
+                r.should.have.property('rev')
+                r.should.have.property('id',testdoc._id)
+                done()
+            })
         })
 
     })
